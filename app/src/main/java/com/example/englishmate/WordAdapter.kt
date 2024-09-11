c6995570fec23bda0779ef3e4239cc1df520ba02
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2024-09-11 15:07:04 +0300

    Move assets folder to correct path

--- app/src/main/java/com/example/englishmate/WordAdapter.kt
+++ app/src/main/java/com/example/englishmate/WordAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.englishmate
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -9,18 +10,19 @@ import com.example.englishmate.databinding.ItemWordsBinding
 class WordAdapter(val wordList: List<Word>) : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
 
 
-    inner class WordViewHolder(private val binding: ItemWordsBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class WordViewHolder(val binding: ItemWordsBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemWordsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return WordViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return wordList.size
     }
 
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       holder.binding.tvWord.text = wordList[position].english
     }
 
 
